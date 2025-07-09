03f9a969681ca290b14fcb414b480ceb269c19a8
strdeok <ejraks1548@gmail.com>
2025-07-09 19:59:11 +0900

    초기 화면 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,10 @@
-import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-full flex flex-col items-center px-4 py-12 justify-between">
-      <img src="/logo.png" className="w-80" />
-      <div className="w-full text-4xl leading-14 font-normal">
-        보고
-        <br />
-        고르고
-        <br />
-        배우는
-        <br />
-        영어영상 학습장
-        <br />
-        <span className="font-semibold">UNMUTE</span>
-      </div>
-      <Link
-        href="/login"
-        className="bg-[#F6BF53] text-white w-full h-12 rounded-3xl flex justify-center items-center"
-      >
-        시작하기
-      </Link>
+    <div className="h-full flex flex-col items-center justify-center gap-4">
+      <Image src="/logo.png" alt="로고이미지" width={200} height={100}></Image>
+      <span className="w-12 h-12 border-4 border-[#F6BF53] border-b-transparent rounded-full inline-block box-border animate-spin" />
     </div>
   );
 }
