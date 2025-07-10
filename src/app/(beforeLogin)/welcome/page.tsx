16db6c8cde1814d9ca457e708cc23cd41645643b
strdeok <ejraks1548@gmail.com>
2025-07-10 21:10:44 +0900

    리팩터링 완료

---
+++ src/app/(beforeLogin)/welcome/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import Image from "next/image";
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+export default function Welcome() {
+  return (
+    <motion.div
+      key="welcome"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className="h-full flex flex-col items-center px-4 py-12 justify-between"
+    >
+      <motion.div layoutId="logo">
+        <Image src="/logo.png" alt="로고이미지" width={350} height={100} />
+      </motion.div>
+      <div className="w-full text-4xl leading-14 font-normal">
+        보고
+        <br />
+        고르고
+        <br />
+        배우는
+        <br />
+        영어영상 학습장
+        <br />
+        <span className="font-semibold">UNMUTE</span>
+      </div>
+      <Link
+        href="/login"
+        className="bg-[#F6BF53] text-white w-full h-12 rounded-3xl flex justify-center items-center"
+      >
+        시작하기
+      </Link>
+    </motion.div>
+  );
+}
