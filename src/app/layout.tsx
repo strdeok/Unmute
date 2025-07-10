d747678866afaf2477dc39505f23b57964a4a4f9
strdeok <ejraks1548@gmail.com>
2025-07-10 21:07:34 +0900

    로딩 환경 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 export const metadata: Metadata = {
   title: "Unmute",
   description: "빠른 영어 학습을 위한 Unmute",
   icons: {
     icon: "/favicon.png",
-  }
+  },
 };
 
 export default function RootLayout({
@@ -16,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="sm:w-96">{children}</body>
+      <body className="sm:w-96">
+        <Suspense fallback={<Loading />}>{children}</Suspense>
+      </body>
     </html>
   );
 }
---
+++ src/app/loading.tsx
@@ -0,0 +1,5 @@
+export default function Loading() {
+  return (
+    <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-12 h-12 border-4 border-[#F6BF53] border-b-transparent rounded-full inline-block box-border animate-spin" />
+  );
+}
