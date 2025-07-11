3de145f3bf49a0c27c30e0072bda7e92627e22ae
strdeok <ejraks1548@gmail.com>
2025-07-11 19:50:13 +0900

    firebase 설치

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERINGID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const analytics = app.name === "[DEFAULT]" ? getAnalytics(app) : null;
+const auth = getAuth(app);
+
+export { app, analytics, auth };
