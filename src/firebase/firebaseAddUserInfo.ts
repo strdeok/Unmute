81b861a040f35b9f54429489f26180d6715c5f1d
strdeok <ejraks1548@gmail.com>
2025-07-19 23:08:48 +0900

    회원 타입 추가

--- src/firebase/firebaseAddUserInfo.ts
+++ src/firebase/firebaseAddUserInfo.ts
@@ -1,12 +1,18 @@
 import { doc, setDoc } from "firebase/firestore";
 import { db } from "./firebase";
 
-export default async function firebaseAddUserInfo(name:string, email:string, phone:string, uid:string) {
+export default async function firebaseAddUserInfo(
+  name: string,
+  email: string,
+  phone: string,
+  uid: string
+) {
   try {
     await setDoc(doc(db, "profile", uid), {
       name,
       email,
       phone,
+      type: "user",
       createdAt: new Date(),
       img: "https://cdn-icons-png.flaticon.com/512/3106/3106921.png",
     });
