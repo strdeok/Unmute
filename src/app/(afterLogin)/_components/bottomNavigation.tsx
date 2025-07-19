842661931f140eabacc5c8c3bbc776ac7b52cc45
strdeok <ejraks1548@gmail.com>
2025-07-19 23:13:51 +0900

    바텀내비게이션 변경

--- src/app/(afterLogin)/_components/bottomNavigation.tsx
+++ src/app/(afterLogin)/_components/bottomNavigation.tsx
@@ -1,35 +1,67 @@
 "use client";
+import HomeIcon from "@/assets/home";
+import SearchIcon from "@/assets/search";
+import UserIcon from "@/assets/user";
+import VideoIcon from "@/assets/video";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
-const navItems = [
-  { href: "/main", label: "메인" },
-  { href: "/search", label: "강의찾기" },
-  { href: "/lecture", label: "내 강의" },
-  { href: "/profile", label: "프로필" },
-];
 
 export default function BottomNavigation() {
   const pathname = usePathname();
 
   return (
     <div className="fixed bottom-0 w-full bg-white flex flex-row items-center justify-between px-4 py-2.5">
-      {navItems.map(({ href, label }) => {
-        const isActive = pathname === href;
+      <Link
+        key="main"
+        href="/main"
+        className={`flex flex-col items-center text-sm flex-1 ${
+          pathname === "/main"
+            ? "text-[#F6BF53] font-semibold"
+            : "text-[#737373]"
+        }`}
+      >
+        <HomeIcon fill={pathname === "/main" ? "#F6BF53" : "#737373"} />
+        <span>메인</span>
+      </Link>
 
-        return (
-          <Link
-            key={href}
-            href={href}
-            className={`flex flex-col items-center text-sm ${
-              isActive ? "text-[#F6BF53] font-semibold" : "text-[#737373]"
-            }`}
-          >
-            <div className="size-4 bg-black rounded-full mb-1" />
-            {label}
-          </Link>
-        );
-      })}
+      <Link
+        key="search"
+        href="/search"
+        className={`flex flex-col items-center text-sm flex-1 ${
+          pathname === "/search"
+            ? "text-[#F6BF53] font-semibold"
+            : "text-[#737373]"
+        }`}
+      >
+        <SearchIcon fill={pathname === "/search" ? "#F6BF53" : "#737373"} />
+        <span>강의찾기</span>
+      </Link>
+
+      <Link
+        key="mycourses"
+        href="/mycourses"
+        className={`flex flex-col items-center text-sm flex-1 ${
+          pathname === "/lecture"
+            ? "text-[#F6BF53] font-semibold"
+            : "text-[#737373]"
+        }`}
+      >
+        <VideoIcon fill={pathname === "/lecture" ? "#F6BF53" : "#737373"} />
+        <span>내 강의</span>
+      </Link>
+
+      <Link
+        key="profile"
+        href="/profile"
+        className={`flex flex-col items-center text-sm flex-1 ${
+          pathname === "/profile"
+            ? "text-[#F6BF53] font-semibold"
+            : "text-[#737373]"
+        }`}
+      >
+        <UserIcon fill={pathname === "/profile" ? "#F6BF53" : "#737373"} />
+        <span>프로필</span>
+      </Link>
     </div>
   );
 }
