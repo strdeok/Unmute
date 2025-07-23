988322488e1cb012db649e5ccaee08e42748196e
strdeok <ejraks1548@gmail.com>
2025-07-23 14:01:26 +0900

    아이콘 추가

---
+++ src/assets/down.tsx
@@ -0,0 +1,8 @@
+export default function DownIcon(){
+    return(
+        <svg width="32" height="24" viewBox="0 0 32 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M24 9L16 15L8 9" stroke="black" strokeWidth="2"/>
+</svg>
+
+    )
+}
