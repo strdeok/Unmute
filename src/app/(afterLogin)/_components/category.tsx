0b4b8eb498c5f47fb7c1bd34fcba7997c06fbefe
strdeok <ejraks1548@gmail.com>
2025-09-29 18:39:43 +0900

    css 수정

--- src/app/(afterLogin)/_components/category.tsx
+++ src/app/(afterLogin)/_components/category.tsx
@@ -10,23 +10,25 @@ export default function Category() {
   const level = params.get("level") || "전체";
 
   return (
-    <div className="h-14 flex flex-row flex-nowrap gap-8 text-sm w-md max-w-md overflow-x-scroll hide-scrollbar">
-      {levels.map((item) => {
-        return (
-          <Link
-            key={item}
-            href={item === "전체" ? `/` : `?level=${item}`}
-            className={`border-b-4 font-bold w-16 flex items-center justify-center flex-shrink-0
-              ${
-                item === level
-                  ? "border-b-[#4300FF]"
-                  : "border-b-[#E5E8EB] text-[#737373]"
-              }`}
-          >
-            {item}
-          </Link>
-        );
-      })}
+    <div className="w-screen max-w-md ">
+      <div className="h-14 flex flex-row flex-nowrap gap-8 text-smoverflow-x-scroll hide-scrollbar">
+        {levels.map((item) => {
+          return (
+            <Link
+              key={item}
+              href={item === "전체" ? `/` : `?level=${item}`}
+              className={`border-b-4 font-bold w-16 flex items-center justify-center flex-shrink-0
+               ${
+                 item === level
+                   ? "border-b-[#4300FF]"
+                   : "border-b-[#E5E8EB] text-[#737373]"
+               }`}
+            >
+              {item}
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 }
